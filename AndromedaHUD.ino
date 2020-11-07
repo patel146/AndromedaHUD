d76380ce43b0231a171174a4b17fb497d5bd23b3
Bhavya Patel <72484938+patel146@users.noreply.github.com>
2020-11-07 08:58:52 -0700

    Update AndromedaHUD.ino

--- AndromedaHUD.ino
+++ AndromedaHUD.ino
@@ -1,3 +1,15 @@
+/*************************************************** 
+  This is an example for the BMP085 Barometric Pressure & Temp Sensor
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> https://www.adafruit.com/products/391
+  These displays use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
 #include "U8glib.h"                                              //Include the graphics library.
 #include <Adafruit_BMP085.h>                                     //library for BMP180
 #include <Wire.h>
